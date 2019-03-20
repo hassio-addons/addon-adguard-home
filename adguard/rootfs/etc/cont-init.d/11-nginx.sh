aa079bea3172cd7517fb2c752a0bed8802b71759
Franck Nijhof <frenck@addons.community>
2019-03-20 21:59:10 +0100

    :hammer: Rewrite add-on onto Bashio

--- adguard/rootfs/etc/cont-init.d/11-nginx.sh
+++ adguard/rootfs/etc/cont-init.d/11-nginx.sh
@@ -1,25 +1,22 @@
-#!/usr/bin/with-contenv bash
+#!/usr/bin/with-contenv bashio
 # ==============================================================================
 # Community Hass.io Add-ons: AdGuard Home
 # Configures NGINX for use with the AdGuard Home server
 # ==============================================================================
-# shellcheck disable=SC1091
-source /usr/lib/hassio-addons/base.sh
-
 declare certfile
 declare keyfile
 declare port
 
-if hass.config.true 'ssl'; then
+if bashio::config.true 'ssl'; then
     rm /etc/nginx/nginx.conf
     mv /etc/nginx/nginx-ssl.conf /etc/nginx/nginx.conf
 
-    certfile=$(hass.config.get 'certfile')
-    keyfile=$(hass.config.get 'keyfile')
+    certfile=$(bashio::config 'certfile')
+    keyfile=$(bashio::config 'keyfile')
 
     sed -i "s/%%certfile%%/${certfile}/g" /etc/nginx/nginx.conf
     sed -i "s/%%keyfile%%/${keyfile}/g" /etc/nginx/nginx.conf
 fi
 
-port=$(hass.config.get 'admin_port')
+port=$(bashio::config 'admin_port')
 sed -i "s/%%adminport%%/${port}/g" /etc/nginx/nginx.conf
--- adguard/rootfs/etc/cont-init.d/20-config.sh
+++ adguard/rootfs/etc/cont-init.d/20-config.sh
@@ -1,20 +1,17 @@
-#!/usr/bin/with-contenv bash
+#!/usr/bin/with-contenv bashio
 # ==============================================================================
 # Community Hass.io Add-ons: AdGuard Home
 # Handles configuration
 # ==============================================================================
-# shellcheck disable=SC1091
-source /usr/lib/hassio-addons/base.sh
-
 readonly CONFIG="/data/adguard/AdGuardHome.yaml"
 declare port
 
-if ! hass.file_exists "${CONFIG}"; then
+if ! bashio::fs.file_exists "${CONFIG}"; then
     mkdir -p /data/adguard
     cp /etc/adguard/AdGuardHome.yaml "${CONFIG}"
 fi
 
-port=$(hass.config.get "dns_port")
+port=$(bashio::config "dns_port")
 
 yq write --inplace "${CONFIG}" \
     'dns.port' "${port}" \
