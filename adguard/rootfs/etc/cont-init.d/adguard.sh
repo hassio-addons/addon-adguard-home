9928290491163d345916a67cebc69eaaffac682b
Franck Nijhof <frenck@addons.community>
2021-05-20 00:42:16 +0200

    Add support for multiple bind interfaces (#168)

--- adguard/rootfs/etc/cont-init.d/adguard.sh
+++ adguard/rootfs/etc/cont-init.d/adguard.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/with-contenv bashio
+# shellcheck disable=SC2207
 # ==============================================================================
 # Home Assistant Community Add-on: AdGuard Home
 # Handles configuration
 # ==============================================================================
 readonly CONFIG="/data/adguard/AdGuardHome.yaml"
 declare port
-declare host
+declare -a hosts
 
 if ! bashio::fs.file_exists "${CONFIG}"; then
     mkdir -p /data/adguard
@@ -14,10 +15,19 @@ fi
 
 port=$(bashio::addon.port "53/udp")
 yq write --inplace "${CONFIG}" \
-    'dns.port' "${port}" \
-    || hass.exit.nok 'Failed updating AdGuardHome DNS port'
+    dns.port "${port}" \
+    || bashio::exit.nok 'Failed updating AdGuardHome DNS port'
 
-host=$(bashio::network.ipv4_address)
-yq write --inplace "${CONFIG}" \
-    'dns.bind_host' "${host%/*}" \
-    || hass.exit.nok 'Failed updating AdGuardHome host'
+# Clean up old interface bind formats
+yq delete --inplace "${CONFIG}" dns.bind_host
+yq delete --inplace "${CONFIG}" dns.bind_hosts
+
+hosts+=($(bashio::network.ipv4_address))
+hosts+=($(bashio::network.ipv6_address))
+hosts+=($(bashio::addon.ip_address))
+for host in "${hosts[@]}"; do
+    bashio::log.info "Adding ${host}"
+    yq write --inplace "${CONFIG}" \
+        dns.bind_hosts[+] "${host%/*}" \
+        || bashio::exit.nok 'Failed updating AdGuardHome host'
+done
